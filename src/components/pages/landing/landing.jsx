e6158898a14e965d6c5456ce8d419dbd28fff8be
Florin <florinbizgan2000@gmail.com>
2024-04-21 17:02:46 +0100

    Add LandingPage component and its related files

---
+++ src/components/pages/landing/landing.jsx
@@ -0,0 +1,10 @@
+import styles from './landing.module.css';
+
+export const LandingPage = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <h1>Landing Page</h1>
+      <p>This is the landing page</p>
+    </div>
+  );
+};
