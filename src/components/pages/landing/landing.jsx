f01b4241f11f56e555fa9785e503be7967666e77
Florin <florinbizgan2000@gmail.com>
2024-04-21 23:05:04 +0100

    Refactor landing.jsx component to improve description rendering

--- src/components/pages/landing/landing.jsx
+++ src/components/pages/landing/landing.jsx
@@ -8,15 +8,11 @@ export const LandingPage = () => {
         return (
           <div className={styles.bodyContainer} key={id}>
             <h1>{title}</h1>
-
-            {/* If description is a string, render as a single paragraph  */}
             {Array.isArray(description) ? (
-              // If description is an array, map through it
               description.map((paragraph, index) => {
                 return <p key={index}>{paragraph}</p>;
               })
             ) : (
-              // If description is not an array, render it as a single paragraph
               <p>{description}</p>
             )}
             <button>{cta}</button>
